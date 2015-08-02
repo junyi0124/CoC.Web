b6d4fa19f45b1d1bb8e0b8282af8f7b2de098c14
张君毅 <zhangjunyi@hatech.com.cn>
2015-08-02 10:53:08 +0800

    initial

---
+++ CoC.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoC.Web.MvcApplication" Language="C#" %>
---
+++ CoC.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoC.Web.Startup))]
+namespace CoC.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
